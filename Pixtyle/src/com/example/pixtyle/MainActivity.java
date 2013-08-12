6d5d40011f35115f45eca55955f2b0fe00f788ba
sasmita <spatra@pdx.edu>
2013-08-12 04:10:22 -0700

    Update MainActivity.java

--- Pixtyle/src/com/example/pixtyle/MainActivity.java
+++ Pixtyle/src/com/example/pixtyle/MainActivity.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright © 2013 Sisinty Sasmita Patra
+ * Pixtyle is an Open source android application where you can add different styles to your pictures.
+ * This Program is a free software: you can redistribute it and/or modify under the terms of the GNU 
+ * General Public License as published by the Free Software Foundation, either version 3 of the 
+ * license or, at your option any later version. This Program is distributed in the hope that it 
+ * will be useful, but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * See the GNU GENERAL PUBLIC LICENSE for more details: http://www.gnu.org/licenses/.
+ * Please see the License file in this distribution for license terms.
+ * Link to the License file: http://github.com/sasmita/Pixtyle/blob/master/License.txt
+ *
+ * Author: Sisinty Sasmita Patra
+ * Email:  spatra@pdx.edu
+ * Repository Link: https://github.com/sasmita/Pixtyle
+ */
 package com.example.pixtyle;
 
 import android.app.Activity;
